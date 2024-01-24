21f6433a3e650c8cc9a9d4fa8e591fd811575a8e
shih-tzuco <annuschk@gmail.com>
2024-01-24 16:55:21 +0200

    Initial commit

---
+++ src/main/java/step/learning/Basics.java
@@ -0,0 +1,23 @@
+package step.learning;
+
+public class Basics {
+    public void run() {
+        System.out.println("Java basics");
+        // Змінні та типи данних
+        // є прімітиви, які поводяться як значимі  типи
+        // всі інші даних - reference
+        // одразу виделяється память 10
+        int x = 10;
+        Integer xx = 10; // reference - аналог int
+    }
+    private void unused(){
+
+    }
+}
+
+/*
+повністю встановити джава, засоби розробніка, IDE
+Створити проєкт за Maven quickstart
+Налаштувати Git, опуублікувати репозиторій
+Прикласти посилання на репозіторий проєкту
+ */
